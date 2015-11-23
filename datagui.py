86d3c4abb86cc0b378c0c4281a965139beaad23c
court-johnson <court.landry@gmail.com>
2015-11-22 20:20:04 -0600

    Database
    Pulls data from the database and allows for new data to be added

--- datagui.py
+++ datagui.py
@@ -30,12 +30,28 @@ class Frame(wx.Frame):
         # Single line text boxes
         self.sName = wx.TextCtrl(panel, size=(150, -1), pos=(130,70))
         self.sGen = wx.TextCtrl(panel, size=(150, -1), pos=(130,110))
-        self.sAge = wx.SpinCtrl(panel, value='0', pos=(130,150), size=(70,25))
+        self.sAge = wx.SpinCtrl(panel, value='0', pos=(130, 150), size=(70, 25))
         self.sOcc = wx.TextCtrl(panel, size=(150, -1), pos=(130,190))
         
         # Save button
         save = wx.Button(panel, label="Add Character", pos=(100, 230))
         save.Bind(wx.EVT_BUTTON, self.addCharacter)
+        
+        # Setup the Table UI
+        # Setup table as listCtrl
+        self.listCtrl = wx.ListCtrl(panel, size=(400,400), pos=(350,40), style=wx.LC_REPORT |wx.BORDER_SUNKEN)
+        
+        # Add columns to listCtrl
+        self.listCtrl.InsertColumn(0, "ID")
+        self.listCtrl.InsertColumn(1, "Name")
+        self.listCtrl.InsertColumn(2, "Gender")
+        self.listCtrl.InsertColumn(3, "Age")
+        self.listCtrl.InsertColumn(4, "Occupation")
+        
+        # Add data to the list control
+        self.fillListCtrl()
+
+        
         
     def addCharacter(self, event):
         name = self.sName.GetValue()
@@ -43,19 +59,42 @@ class Frame(wx.Frame):
         age = self.sAge.GetValue()
         occ = self.sOcc.GetValue()
         
-        print name
-        print gen
-        print age
-        print occ
-
-        sqprac.newCharacter(name,gen,age,occ)
+        # Checking if variables have a value
+        if (name == '') or (gen == '') or (age == '') or (occ == ''):
+            # Alert user that a variable is empty
+            dlg = wx.MessageDialog(None, \
+                'Some character details are missing. Enter values in each text box.', \
+                'Missing Details', wx.OK)
+            dlg.ShowModal()
+            dlg.Destroy()
+            
+            return False
+        
+        # Adding character to database
+        sqprac.newCharacter(name, gen, age, occ)
         print sqprac.viewAll()
+        
+        # Empty text boxes when finished.
+        self.sName.Clear()
+        self.sGen.Clear()
+        self.sOcc.Clear()
+        self.sAge.SetValue(0)
+
+        self.fillListCtrl()
     
     def exitProgram(self, event):
         self.Destroy()
-    
-    
-    
+        
+    def fillListCtrl(self):
+        # Get data from the database
+        allData = sqprac.viewAll()
+        self.listCtrl.DeleteAllItems()
+        
+        # Append data to the table
+        for row in allData:
+            # Loop though and append data
+            self.listCtrl.Append(row)
+        
 app = wx.App()
 frame = Frame("Python GUI")
 frame.Show()
